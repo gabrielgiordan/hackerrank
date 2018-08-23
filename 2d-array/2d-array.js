2f0fd09f6b6e76052e5b5b209c8e4b4d0286cc0e
Gabriel Giordano <git@gabrielgiordano.com>
2018-08-23 16:53:32 -0300

    2d-array

--- 2d-array/2d-array.js
+++ 2d-array/2d-array.js
@@ -1,8 +1,31 @@
 'use strict';
 
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
 // Complete the hourglassSum function below.
 function hourglassSum(arr) {
-
     let hourglasses = []
 
     for (let i = 0; i + 2 < arr.length; i++) {
@@ -34,4 +57,20 @@ function hourglassSum(arr) {
     }
 
     return maxSum
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    let result = hourglassSum(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
 }
