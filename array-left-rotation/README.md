66f57fc0393752f03b3951ba1f947eb1fc1dc04c
Gabriel Giordano <git@gabrielgiordano.com>
2018-08-23 18:15:43 -0300

    array-left-rotation.js

---
+++ array-left-rotation/README.md
@@ -0,0 +1,18 @@
+# Arrays: Left Rotation
+
+### Output Format
+
+Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
+
+### Sample Input
+
+```
+5 4
+1 2 3 4 5
+```
+
+### Sample Output
+
+```
+5 1 2 3 4
+```
---
+++ array-left-rotation/array-left-rotation.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    let r = []
+
+    d %= a.length
+
+    for (let i = 0; i < a.length; ++i) {
+        r[i] = a[(i + d) % a.length]
+    }
+
+    return r
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    console.log(process.env.OUTPUT_PATH)
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
