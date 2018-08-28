470f3f00561a4ad0d7b7383204ed45504547cb2c
Gabriel Giordano <git@gabrielgiordano.com>
2018-08-28 15:59:15 -0300

    s10-the-central-limit-theorem

---
+++ 10-days-of-statistics/s10-the-central-limit-theorem-3/s10-the-central-limit-theorem-3.js
@@ -0,0 +1,23 @@
+function processData(input) {
+    //Enter your code here
+    
+    input = input.split('\n')
+    let mx = parseFloat(input[0]),
+        m = parseFloat(input[1]),
+        sd = parseFloat(input[2]),
+        i = parseFloat(input[3]),
+        z = parseFloat(input[4])
+
+    function confidenceInterval(z, sd, mx) {
+        return z * (sd / Math.sqrt(mx))
+    }
+
+    console.log((m - confidenceInterval(z, sd, mx)).toFixed(2))
+    console.log((m + confidenceInterval(z, sd, mx)).toFixed(2))
+}
+
+processData(`100
+500
+80
+.95
+1.96`)
