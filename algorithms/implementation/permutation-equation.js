78f30d7c735ff0158f08b5340a179845f8eab6a9
Anthony Nandaa <profnandaa@gmail.com>
2018-10-18 19:37:06 +0300

    feat: alternative implementation for permulation-equation
    This introduces an alternative implementation for the
    permutationEquation function; with a better run-time
    but with extra space.

--- algorithms/implementation/permutation-equation.js
+++ algorithms/implementation/permutation-equation.js
@@ -11,3 +11,20 @@ function permutationEquation(p) {
 console.log(permutationEquation([5,2,1,3,4])) // 4,2,5,1,3
 console.log(permutationEquation([2,3,1])) // 2,3,1
 console.log(permutationEquation([4,3,5,1,2])) // 1,3,5,4,2
+
+/**
+ * Alternative using map
+ * with O(1) look-up
+ */
+function permutationEquationV2(p) {
+  const map = {}
+  for (let i = 0; i < p.length; i += 1) {
+    map[p[i]] = i + 1
+  }
+
+  return Object.values(map).map(k => map[k])
+}
+
+console.log(permutationEquationV2([5,2,1,3,4])) // 4,2,5,1,3
+console.log(permutationEquationV2([2,3,1])) // 2,3,1
+console.log(permutationEquationV2([4,3,5,1,2])) // 1,3,5,4,2
